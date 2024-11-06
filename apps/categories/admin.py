9c86e9087cc20e0fa1d2e5a7ec1eebf4a2a7d1b3
Juan Luna <juandluna17@gmail.com>
2024-11-06 07:55:09 -0500

    Admin for categories

--- apps/categories/admin.py
+++ apps/categories/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from.models import Category
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin): 
+    list_display = ('id', 'name', 'created_at', 'updated_at')
+    search_fields = ('name',)
+    list_filter = ('created_at', 'updated_at')
+    
+
+admin.site.register(Category, CategoryAdmin)
--- apps/categories/models.py
+++ apps/categories/models.py
@@ -8,8 +8,11 @@ class Category(models.Model):
     color_text = models.CharField(max_length=7)
     color_bg = models.CharField(max_length=7)
     logo: str = models.CharField(max_length=255)
-    image: str = models.CharField(max_length=255)
-    imageBig: str = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='category_images/')
+    imageBig = models.ImageField(upload_to='category_images/')
     area_id: int = models.ForeignKey(Area, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name_plural = 'categories'
+        verbose_name = 'category'
