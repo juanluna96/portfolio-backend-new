4066aa62d33d9628335049fdad7948773534d718
Juan Luna <juandluna17@gmail.com>
2024-11-06 07:55:36 -0500

    Admin for contact

--- apps/contact/admin.py
+++ apps/contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Contact
 # Register your models here.
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'phone', 'created_at', 'updated_at')
+    search_fields = ('name', 'email', 'phone')
+    list_filter = ('created_at', 'updated_at')
+
+admin.site.register(Contact, ContactAdmin)
--- apps/contact/models.py
+++ apps/contact/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    country = models.CharField(max_length=100)
+    country_code = models.CharField(max_length=10)
+    phone = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=255)
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+    favorite = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
