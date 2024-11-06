b2b290a7f88327ff6f5f7792999a2ffdba9a5d09
Juan Luna <juandluna17@gmail.com>
2024-11-06 07:55:24 -0500

    Admin for companies

--- apps/companies/admin.py
+++ apps/companies/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from.models import Company
 
 # Register your models here.
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'position', 'image', 'created_at', 'updated_at')
+    search_fields = ('name', 'position')
+    list_filter = ('created_at', 'updated_at')
+    
+
+admin.site.register(Company, CompanyAdmin)
--- apps/companies/models.py
+++ apps/companies/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Company(models.Model):
     name: str = models.CharField(max_length=100)
     position: str = models.CharField(max_length=100)
-    image: str = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='company_images/')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'companies'
+        verbose_name = 'company'
