ec4c3f2daed0cf5ff2f05dbd5d5d7e50369583ee
Juan Luna <juandluna17@gmail.com>
2024-11-06 07:54:23 -0500

    Admin for area

--- apps/areas/admin.py
+++ apps/areas/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from.models import Area
 
 # Register your models here.
+class AreaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_at', 'updated_at')
+    search_fields = ('name',)
+    list_filter = ('created_at', 'updated_at')
+
+admin.site.register(Area, AreaAdmin)
