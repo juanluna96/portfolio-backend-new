98451d2073bcd1cc7d81efde114fc01e81441716
Juan Luna <juandluna17@gmail.com>
2024-11-07 21:28:18 -0500

    Adding description selarizer in project

--- apps/projects/serializer.py
+++ apps/projects/serializer.py
@@ -3,20 +3,31 @@ from apps.images_projects.models import ImageProject
 from apps.projects.models import Project, ProjectDescription
 
 class ProjectDescriptionSerializer(serializers.ModelSerializer):
-    language = serializers.CharField(source='language.name')
+    language = serializers.CharField(source='language.abbreviation')
     
     class Meta:
         model = ProjectDescription
         fields = ['language', 'description']
 class ProjectSerializer(serializers.ModelSerializer):
     language = serializers.StringRelatedField()  # Muestra el nombre del lenguaje como un campo
-    images = serializers.StringRelatedField(many=True)
+    images = serializers.SerializerMethodField()
+    descriptions = ProjectDescriptionSerializer(many=True)  # Muestra las descripciones de cada proyecto como un campo
     categories = serializers.StringRelatedField(many=True)
-    company = serializers.StringRelatedField()
+    company = serializers.SerializerMethodField()
 
     class Meta:
         model = Project
-        fields = ['id', 'title', 'description', 'url', 'language', 'images', 'categories', 'company', 'created_at', 'updated_at']
+        fields = ['id', 'title', 'url', 'language', 'images', 'categories', 'company', 'descriptions']
+        
+    def get_images(self, obj):
+        request = self.context.get('request')
+        if request:
+            # Construir la URL completa con el esquema y el dominio
+            return [request.build_absolute_uri(image.image.url) for image in obj.images.all()]
+        return []
+    
+    def get_company(self, obj):
+        return obj.company.name
 
 class ProjectImageSerializer(serializers.ModelSerializer):
     class Meta:
