dc525ebad8fc622fcac919710b65ae95682a1adc
Juan Luna <juandluna17@gmail.com>
2026-05-11 13:19:56 -0500

    fix: configurar SSL de base de datos como opcional y agregar nuevas migraciones

--- main/settings.py
+++ main/settings.py
@@ -139,7 +139,7 @@ DATABASES = {
     'default': dj_database_url.parse(
         f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}",
         conn_max_age=600,
-        ssl_require=True
+        ssl_require=os.getenv('DB_SSL_REQUIRE', 'True').lower() in ('true', '1', 'yes')
     )
 }
 
