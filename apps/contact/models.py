336a61c9cfbb6c81d44f260109437723629f308a
Juan Luna <juandluna17@gmail.com>
2026-05-27 11:41:22 -0500

    fix(contact): update contact model fields to be optional

--- apps/contact/models.py
+++ apps/contact/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    country = models.CharField(max_length=100)
-    country_code = models.CharField(max_length=10)
-    phone = models.CharField(max_length=20)
+    country = models.CharField(max_length=100, blank=True, default='')
+    country_code = models.CharField(max_length=10, blank=True, default='')
+    phone = models.CharField(max_length=20, blank=True, default='')
     address = models.CharField(max_length=255, blank=True)
     message = models.TextField()
     read = models.BooleanField(default=False)
